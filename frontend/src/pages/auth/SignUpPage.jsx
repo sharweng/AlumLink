fcb0ac795873aba84cd9c67c25c51a45a2cdd167
sharweng <162144450+sharweng@users.noreply.github.com>
2025-09-24 00:48:42 +0800

    sign up page
    - installed lucide-react package for icons
    - inserted color scheme in tailwind.config.js
    - created signuppage and signup form for that page
    - used alumnilinknowhite png for image in sign up page

--- frontend/src/pages/auth/SignUpPage.jsx
+++ frontend/src/pages/auth/SignUpPage.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import SignUpForm from '../../components/auth/SignUpForm'
+import { Link } from 'react-router-dom'
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <div className='min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+            <img className='mx-auto h-36 w-auto' src="/alumniLinkNoWhite.png" alt="AlumniLinkLogo" />
+            {/* h2 - tagline change it? */}
+            <h2 className='text-center text-3xl font-extrabold text-gray-900'>
+                Connect with familiar faces, AlumniLink
+            </h2>
+        </div>
+        <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md shadow-md'>
+            <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+                <SignUpForm />
+                
+                <div className='mt-6'>
+                    <div className='relative'>
+                        <div className='absolute inset-0 flex items-center'>
+                            <div className='w-full border-t border-gray-300'></div>
+                        </div>
+                        <div className='relative flex justify-center text-sm'>
+                            <span className='px-2 bg-white text-gray-500'>Already in AlumniLink?</span>
+                        </div>
+                    </div>
+                    <div className='mt-6'>
+                        <Link to='/login' className='w-full flex justify-center py-2 px-4 border border-transparent rounden-md shadow-sm text-sm font-medium text-primary bg-white hover:bg-gray-50'>
+                            Sign in
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,4 +9,19 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [{
+      alumnilink: {
+        "primary": "#D62F2F",
+        "secondary": "#FFFFFF",
+        "accent": "#FF7F7F",
+        "neutral": "#000000",
+        "base-100": "#FFEDED",
+        "info": "#5E5E5E",
+        "success": "#057642",
+        "warning": "#F5C75D",
+        "error": "#CC1016"
+      }
+    }]
+  }
 }
