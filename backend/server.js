21bb4cf0f094825103a2831bc053839e9e1b6ee6
sharweng <162144450+sharweng@users.noreply.github.com>
2025-09-23 19:18:18 +0800

    limit images to 5mb

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(express.json())
+app.use(express.json({ limit: "5mb" })) // limits image size to 5mb
 app.use(cookieParser())
 
 app.use("/api/v1/auth", authRoutes)
