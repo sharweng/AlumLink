67c02b868973d062b2d06e9f53e00d0928544666
sharweng <162144450+sharweng@users.noreply.github.com>
2025-09-26 02:09:59 +0800

    post actions and its functions 2/3
    - can like and unlike posts
    - can add comments to the post
    - installed date-fns package for date text thingy like ago ago

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -3,7 +3,9 @@ import { useState } from "react"
 import { axiosInstance } from "../lib/axios"
 import toast from "react-hot-toast"
 import { Link } from "react-router-dom"
-import { Loader, Trash2 } from "lucide-react"
+import { Heart, Loader, MessageCircle, Send, Share2, Trash2 } from "lucide-react"
+import PostAction from "./PostAction"
+import { formatDistanceToNow } from "date-fns"
 
 const Post = ({ post }) => {
   const queryClient = useQueryClient()
@@ -46,7 +48,6 @@ const Post = ({ post }) => {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["posts"] })
-      toast.success("Post liked successfully")
     },
     onError: (error) => {
       toast.error(error.response.data.message || "Failed to like a post")
@@ -56,6 +57,31 @@ const Post = ({ post }) => {
   const handleDeletePost = () => {
     if(!window.confirm("Are you sure you want to delete this post?")) return
     deletePost()
+  }
+
+  const handleLikePost = async () => {
+    if (isLikingPost) return
+    likePost()
+  }
+
+  const handleAddComment = async (e) => {
+    e.preventDefault()
+    if(newComment.trim()) {
+      createComment(newComment)
+      setNewComment("")
+      setComments([
+        ...comments,
+        {
+          content: newComment,
+          user: {
+            _id: authUser._id,
+            name: authUser.name,
+            profilePicture: authUser.profilePicture
+          },
+          createdAt: new Date()
+        }
+      ])
+    }
   }
 
   return (
@@ -71,7 +97,7 @@ const Post = ({ post }) => {
                 <h3 className="font-semibold">{ post.author.name }</h3>
               </Link>
               <p className="text-xs text-info">{ post.author.headline }</p>
-              {/* todo: add post created at field and format it  */}
+              <p className="text-xs text-info">{ formatDistanceToNow(new Date(post.createdAt), { addSuffix: true }) }</p>
             </div>
           </div>
           {isOwner && (
@@ -82,7 +108,51 @@ const Post = ({ post }) => {
         </div>
         <p className="mb-4">{ post.content }</p>
         { post.image && <img src={ post.image } alt="Post content" className="rounded-lg w-full mb-4" />}
+
+        <div className="flex justify-between text-info">
+          <PostAction
+            icon={<Heart size={18} className={isLiked ? "text-red-500 fill-red-300" : ""} />}
+            text={`Like (${ post.likes.length })`}
+            onClick={ handleLikePost }
+          />
+
+          <PostAction
+            icon={<MessageCircle size={18} />}
+            text={`Comment (${ comments.length })`}
+            onClick={ () => setShowComments(!showComments) }
+          />
+
+          <PostAction icon={<Share2 size={18} />} text='Share' />
+        </div>
       </div>
+      { showComments && (
+        <div className="px-4 pb-4">
+          <div className="mb-4 max-h-60 overflow-y-auto">
+            { comments.map((comment) => (
+              <div key={ comment._id } className="mb-2 bg-base-100 p-2 rounded flex items-start">
+                <img src={ comment.user.profilePicture || "/avatar.png" } alt={ comment.user.name }
+                className="w-8 h-8 rounded-full mr-2 flex-shrink-0" />
+                <div className="flex-grow">
+                  <div className="flex items-center mb-1">
+                    <span className="font-semibold mr-2">{ comment.user.name }</span>
+                    <span className="text-xs text-info">{ formatDistanceToNow(new Date(comment.createdAt)) }</span>
+                  </div>
+                  <p>{ comment.content}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <form onSubmit={ handleAddComment } className="flex items-center">
+            <input type="text" value={ newComment } onChange={(e) => setNewComment(e.target.value)} placeholder="Add a comment..." 
+            className="flex-grow p-2 rounded-l-full bg-base-100 focus:outline-none focus:ring-2 focus:ring-primary"/>
+
+            <button type="submit" className="bg-primary text-white p-2 rounded-r-full hover:bg-primary-dark transition duration-300"
+            disabled={ isCreatingComment }>
+              { isCreatingComment ? <Loader size={18} className="animate-spin" /> : <Send size={18} />}
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
