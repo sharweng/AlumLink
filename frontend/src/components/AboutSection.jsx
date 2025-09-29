f93b93d77b663cba9f1789f4bdbef34eef44f593
sharweng <162144450+sharweng@users.noreply.github.com>
2025-09-29 22:09:22 +0800

    bug fix: about can be seen by all users now

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -12,36 +12,36 @@ const AboutSection = ({ userData, isOwnProfile, onSave}) => {
   return (
     <div className='bg-white shadow rounded-lg p-6 mb-6'>
 			<h2 className='text-xl font-semibold mb-4'>About</h2>
-			{isOwnProfile && (
-				<>
-					{isEditing ? (
-						<>
-							<textarea
-								value={about}
-								onChange={(e) => setAbout(e.target.value)}
-								className='w-full p-2 border rounded'
-								rows='4'
-							/>
-							<button
-								onClick={handleSave}
-								className='mt-2 bg-primary text-white py-2 px-4 rounded hover:bg-red-700 
-								transition duration-300'
-							>
-								Save
-							</button>
-						</>
-					) : (
-						<>
-							<p>{userData.about}</p>
-							<button
-								onClick={() => setIsEditing(true)}
-								className='mt-2 text-primary hover:text-red-700 transition duration-300'
-							>
-								Edit
-							</button>
-						</>
-					)}
-				</>
+			{isOwnProfile ? (
+				isEditing ? (
+					<>
+						<textarea
+							value={about}
+							onChange={(e) => setAbout(e.target.value)}
+							className='w-full p-2 border rounded'
+							rows='4'
+						/>
+						<button
+							onClick={handleSave}
+							className='mt-2 bg-primary text-white py-2 px-4 rounded hover:bg-red-700 
+							transition duration-300'
+						>
+							Save
+						</button>
+					</>
+				) : (
+					<>
+						<p>{userData.about}</p>
+						<button
+							onClick={() => setIsEditing(true)}
+							className='mt-2 text-primary hover:text-red-700 transition duration-300'
+						>
+							Edit
+						</button>
+					</>
+				)
+			) : (
+				<p>{userData.about}</p>
 			)}
 		</div>
   )
